1ed81ace673ae7ae27646cb5ff8854715abe0d77
Dylan Sherwood <dylansherwood35@gmail.com>
2021-09-05 22:19:53 +0700

    Homework

--- app/src/main/java/ic/ac/ubaya/informatika/advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/ic/ac/ubaya/informatika/advweek4/view/StudentDetailFragment.kt
@@ -5,10 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import ic.ac.ubaya.informatika.advweek4.R
+import ic.ac.ubaya.informatika.advweek4.viewmodel.DetailViewModel
+import ic.ac.ubaya.informatika.advweek4.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.fragment_student_detail.*
+import kotlinx.android.synthetic.main.fragment_student_list.*
+import kotlinx.android.synthetic.main.student_list_item.*
+import kotlinx.android.synthetic.main.student_list_item.txtId
+import kotlinx.android.synthetic.main.student_list_item.txtName
 
 
 class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel:DetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +31,17 @@ class StudentDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+        observeViewModel()
+    }
+
+    fun observeViewModel(){
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            txtId.setText(viewModel.studentLD.value?.id)
+            txtName.setText(viewModel.studentLD.value?.name)
+            txtBod.setText(viewModel.studentLD.value?.bod)
+            txtPhone.setText(viewModel.studentLD.value?.phone)
+        })
     }
 }
